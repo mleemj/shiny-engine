2b3846e92975cde1512330ab1802b05d85e24433
mleemj <mleemj@gmail.com>
2017-03-23 12:32:21 -0500

    Stitch demo
    Stitch demo

--- salmon.py
+++ salmon.py
@@ -1,27 +1,14 @@
-from flask import Flask, render_template, request, jsonify, Response, \
-    stream_with_context
-
-from Max_Sales_Strategy import MaxSalesStrategy
-
 from itertools import product
+
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-#
-#
-# @app.route('/_add_numbers')
-# def add_numbers():
-#     a = request.args.get('a', 0, type=int)
-#     b = request.args.get('b', 0, type=int)
-#     return jsonify(result=a + b)
-
 @app.route('/')
 def index():
-    return render_template('max_sales_strategy.html')
+    return render_template('sales_strategy.html')
+
 
 @app.route('/_curate')
 def max_sales_strategy():
@@ -33,20 +20,6 @@ def max_sales_strategy():
         rdict[index] = list(combination)
         index += 1
     return jsonify(rdict)
-
-# maxSales = MaxSalesStrategy(num_of_collections)
-# list_combinations = maxSales.get_list_combinations()
-# return Response(stream_template('max_sales_strategy.html',
-#                                 combinations_list=list_combinations))
-
-
-@stream_with_context
-def stream_template(template_name, **context):
-    app.update_template_context(context)
-    template = app.jinja_env.get_template(template_name)
-    templateStream = template.stream(context)
-    templateStream.enable_buffering(5)
-    return templateStream
 
 
 if __name__ == '__main__':
